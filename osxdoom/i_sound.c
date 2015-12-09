5cc3d99c21c2e4dd6e08b7b474b7270f9bbb4ed8
John D <redline@Johns-MacBook-Air.local>
2015-12-09 07:21:07 -0600

    Removed linux/soundcard.h - may need to port to OSX's CoreAudio.h API. Also, installed Quartz to get X11 Window support back.  Finally added explicit include path to symlinked location of Quartz X11 includes.

--- osxdoom/i_sound.c
+++ osxdoom/i_sound.c
@@ -42,7 +42,8 @@ rcsid[] = "$Id: i_unix.c,v 1.5 1997/02/03 22:45:10 b1 Exp $";
 #include <sys/ioctl.h>
 
 // Linux voxware output.
-#include <linux/soundcard.h>
+//PORTJD - not available on OSX
+//#include <linux/soundcard.h>
 
 // Timer stuff. Experimental.
 #include <time.h>
